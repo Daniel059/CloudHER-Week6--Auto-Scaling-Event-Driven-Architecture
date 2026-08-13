6d2da658493cd0bb454e4b98b40d7d5413a33043
Daniel Nzioki <danzioki01@gmail.com>
2026-08-13 16:58:12 +0300

    Edited my ec2 launch template

--- ec2-launch-template/user_data.sh
+++ ec2-launch-template/user_data.sh
@@ -3,8 +3,11 @@ dnf update -y
 dnf install httpd -y
 systemctl enable httpd
 systemctl start httpd
-INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)
-AZ=$(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
+TOKEN=$(curl -s -X PUT "http://169.254.169.254/latest/api/token" -H "X-aws-ec2-metadata-token-ttl-seconds: 21600")
+INSTANCE_ID=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/instance-id)
+AZ=$(curl -s -H "X-aws-ec2-metadata-token: $TOKEN" http://169.254.169.254/latest/meta-data/placement/availability-zone)
+
 cat > /var/www/html/index.html <<EOF2
 <!DOCTYPE html>
 <html>
